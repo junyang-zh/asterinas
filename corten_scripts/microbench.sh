55b55faf698e802b0c5bcaf521d2fdb9b5cdb7e0
Zhang Junyang <junyang@stu.pku.edu.cn>
2024-11-24 17:57:07 +0800

    Add virtual memory system call scalability microbenchmarks
    Modify the micro-benchmark according to the design
    
    Add the old microbenchmark
    
    Add the pin-cpu script

---
+++ corten_scripts/microbench.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# args: microbench.sh [linux/asterinas]
+
+set -e
+
+export QMP_PORT=3336
+
+NR_CPUS=384
+
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+PIN_CPU_SCRIPT="$SCRIPT_DIR/pin_cpu.py"
+TEST_RESULTS_DIR="$SCRIPT_DIR/../test_results"
+TMUX_SESSION_NAME="microbench_session"
+mkdir -p "$TEST_RESULTS_DIR"
+
+BENCH_TARGET=$1
+if [ "$BENCH_TARGET" == "linux" ]; then
+    START_VM_CMD="$SCRIPT_DIR/start_linux.sh $NR_CPUS"
+    BENCH_OUTPUT_FILE="$TEST_RESULTS_DIR/linux_output.txt"
+    EXIT_COMMAND="; poweroff -f"
+else
+    START_VM_CMD="make run SMP=$NR_CPUS MEM=240G RELEASE_LTO=1"
+    BENCH_OUTPUT_FILE="$TEST_RESULTS_DIR/aster_output.txt"
+    EXIT_COMMAND="; exit"
+fi
+
+pushd "$SCRIPT_DIR/.."
+
+run_microbench() {
+    # Usage: run_microbench <command>
+    COMMAND=$1
+    COMMAND+=$EXIT_COMMAND
+
+    tmux new-session -d -s ${TMUX_SESSION_NAME}
+
+    ASTER_SESSION_KEYS=$START_VM_CMD
+    ASTER_SESSION_KEYS+=" 2>&1 | tee -a ${BENCH_OUTPUT_FILE}"
+    # Exit session when the command finishes
+    ASTER_SESSION_KEYS+="; exit"
+
+    echo "Starting VM in tmux session ${TMUX_SESSION_NAME}:0 with command:"
+    echo "# $ASTER_SESSION_KEYS"
+    tmux send-keys -t ${TMUX_SESSION_NAME}:0 "$ASTER_SESSION_KEYS" Enter
+
+    echo "Wait for \"~ \#\" shell prompt to appear in $BENCH_OUTPUT_FILE"
+    while ! tail -n 1 $BENCH_OUTPUT_FILE | grep -q "~ #"; do
+        echo "Waiting..."
+        sleep 5
+    done
+
+    # Bind cores
+    echo "Binding cores to VM"
+    python3 $PIN_CPU_SCRIPT $QMP_PORT $NR_CPUS
+
+    # Run the microbenchmark
+    echo "Running microbenchmark command: $COMMAND"
+    tmux select-window -t ${TMUX_SESSION_NAME}:0
+    tmux send-keys -t ${TMUX_SESSION_NAME}:0 "${COMMAND}" Enter
+
+    tmux attach -t ${TMUX_SESSION_NAME}:0
+}
+
+if [ -f "$BENCH_OUTPUT_FILE" ]; then
+    rm "$BENCH_OUTPUT_FILE"
+fi
+
+run_microbench "/test/scale/mmap unfixed"
+run_microbench "/test/scale/mmap fixed 1"
+run_microbench "/test/scale/mmap fixed 2"
+run_microbench "/test/scale/mmap_pf unfixed"
+run_microbench "/test/scale/mmap_pf fixed 1"
+run_microbench "/test/scale/mmap_pf fixed 2"
+run_microbench "/test/scale/pf one_vma 0"
+run_microbench "/test/scale/pf one_vma 1"
+run_microbench "/test/scale/pf one_vma 2"
+run_microbench "/test/scale/munmap_virt one_vma 0"
+run_microbench "/test/scale/munmap_virt one_vma 1"
+run_microbench "/test/scale/munmap_virt one_vma 2"
+run_microbench "/test/scale/munmap one_vma 0"
+run_microbench "/test/scale/munmap one_vma 1"
+run_microbench "/test/scale/munmap one_vma 2"
+
+unset QMP_PORT
+
+popd
---
+++ test/apps/scale/munmap.c
@@ -0,0 +1,60 @@
+#include "common.h"
+
+#define NUM_PAGES 32 // Number of pages to unmap per thread
+
+void *worker_thread(void *arg)
+{
+	thread_start();
+
+	// unmap them one by one
+	for (size_t i = 0; i < NUM_PAGES; i++) {
+		munmap(data->base + data->offset[i], PAGE_SIZE);
+	}
+
+	thread_end(NUM_PAGES);
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc > 4 || argc < 3) {
+		fprintf(stderr,
+			"Usage: %s one_vma|multi_vma contention_level [num_threads]\n",
+			argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	int one_multi = 0;
+	int contention_level = 0;
+	int num_threads = -1;
+	if (strcmp(argv[1], "one_vma") == 0) {
+		one_multi = 0;
+	} else if (strcmp(argv[1], "multi_vma") == 0) {
+		one_multi = 1;
+	} else {
+		fprintf(stderr, "Invalid argument: %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	contention_level = atoi(argv[2]);
+	if (contention_level < 0 || contention_level > 2) {
+		fprintf(stderr, "Invalid contention level: %s\n", argv[2]);
+		exit(EXIT_FAILURE);
+	}
+	if (argc == 4) {
+		num_threads = read_num_threads(argv[3]);
+	} else {
+		num_threads = -1;
+	}
+
+	printf("***MUNMAP %s %s***\n", one_multi ? "MULTI_VMAS" : "ONE_VMA",
+	       contention_level_name[contention_level]);
+	run_test_specify_threads(
+		num_threads, worker_thread,
+		(test_config_t){ .num_requests_per_thread = NUM_PAGES,
+				 .num_pages_per_request = 1,
+				 .mmap_before_spawn = 1,
+				 .trigger_fault_before_spawn = 1,
+				 .multi_vma_assign_requests = one_multi,
+				 .contention_level = contention_level,
+				 .is_unfixed_mmap_test = 0 });
+	printf("\n");
+}
