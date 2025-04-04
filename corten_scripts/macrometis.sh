98293fdef9ad82c03418ad5e4c5f6e74dcfd7c34
Zhang Junyang <junyang@stu.pku.edu.cn>
2025-04-04 14:54:53 +0800

    Add MOSBENCH/PARSEC data image and scripts
    [Workaround] shell hangs

---
+++ corten_scripts/macrometis.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# usage: macrometis.sh [linux|asterinas]
+
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+BENCH_SCRIPT="$SCRIPT_DIR/bench.sh"
+CORTEN_OUTPUT_DIR="$SCRIPT_DIR/../corten_outputs"
+mkdir -p "$CORTEN_OUTPUT_DIR"
+
+SYS_NAME=$1
+
+if [ "$SYS_NAME" != "linux" ] && [ "$SYS_NAME" != "aster" ]; then
+    echo "Usage: $0 [linux|aster]"
+    exit 1
+fi
+
+BENCH_OUTPUT_FILE="$CORTEN_OUTPUT_DIR/macrometis_${SYS_NAME}_$(date +%Y%m%d_%H%M%S).log"
+
+THREAD_COUNTS=(1 2 4 8 16 32 64 128 192 256 320 384)
+for THREAD_COUNT in "${THREAD_COUNTS[@]}"; do
+    if [ $THREAD_COUNT -eq 384 ]; then
+        export NR_CPUS=384
+    else
+        export NR_CPUS=$(($THREAD_COUNT + 2))
+    fi
+    $BENCH_SCRIPT $SYS_NAME $BENCH_OUTPUT_FILE "/test/corten_benchmetis.sh $THREAD_COUNT"
+done
