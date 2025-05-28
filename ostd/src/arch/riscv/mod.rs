50e7acf6f35451cd1352561fe5a02020aea96eb7
Zejun Zhao <jelly.zhao.42@gmail.com>
2025-05-28 15:35:00 +0800

    Add RISC-V PLIC support

--- ostd/src/arch/riscv/mod.rs
+++ ostd/src/arch/riscv/mod.rs
@@ -9,7 +9,7 @@ pub(crate) mod cpu;
 pub mod device;
 mod io;
 pub(crate) mod iommu;
-pub(crate) mod irq;
+pub mod irq;
 pub(crate) mod mm;
 pub(crate) mod pci;
 pub mod qemu;
@@ -20,7 +20,9 @@ pub mod trap;
 
 use core::sync::atomic::Ordering;
 
-use crate::arch::timer::TIMER_IRQ_NUM;
+use irq::IRQ_CHIP;
+
+use crate::{arch::timer::TIMER_IRQ_NUM, cpu::CpuId};
 
 #[cfg(feature = "cvm_guest")]
 pub(crate) fn init_cvm_guest() {
@@ -41,6 +43,11 @@ pub(crate) unsafe fn init_on_bsp_after_heap() {
     unsafe { trap::init() };
 
     let io_mem_builder = io::construct_io_mem_allocator_builder();
+
+    // SAFETY: This function is called once and at most once here at a proper timing
+    // in the boot context of the BSP, with no irq-related operations having
+    // been performed.
+    unsafe { irq::init(&io_mem_builder) };
 
     // SAFETY: We're on the BSP and we're ready to boot all APs.
     unsafe { crate::boot::smp::boot_all_aps() };
@@ -66,7 +73,11 @@ pub(crate) unsafe fn init_on_bsp_after_heap() {
 /// 1. This function must be called only once in the boot context of the
 ///    bootstrapping processor.
 /// 2. This function should be called after the kernel page table is activated.
-pub(crate) unsafe fn init_on_bsp_after_kpt() {}
+pub(crate) unsafe fn init_on_bsp_after_kpt() {
+    // SAFETY: This function is called only once in the boot context of the BSP,
+    // after the kernel page table is activated.
+    unsafe { irq::init_after_kpt() };
+}
 
 pub(crate) unsafe fn init_on_ap() {
     unimplemented!()
@@ -79,7 +90,12 @@ pub(crate) fn interrupts_ack(irq_number: usize) {
         return;
     }
 
-    unimplemented!()
+    // Invoked always in interrupt context, so there's no race condition.
+    IRQ_CHIP
+        .get()
+        .unwrap()
+        .lock()
+        .complete_interrupt(CpuId::current_racy().as_usize() as u32, irq_number as u32);
 }
 
 /// Return the frequency of TSC. The unit is Hz.
