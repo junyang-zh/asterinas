fc4406c309706e5c21f98245c79a194dc80d7fbe
Zhang Junyang <junyang@stu.pku.edu.cn>
2025-09-01 22:16:23 +0800

    Upgrade the `riscv` crate to fix the ssoft handling
    https://github.com/rust-embedded/riscv/commit/3c618636307fb5c9cd8db6c58cf4207e5742b35e

--- ostd/src/arch/riscv/cpu/context.rs
+++ ostd/src/arch/riscv/cpu/context.rs
@@ -6,12 +6,15 @@ use alloc::boxed::Box;
 use core::{arch::global_asm, fmt::Debug};
 
 use ostd_pod::Pod;
-use riscv::register::scause::{Exception, Trap};
+use riscv::{
+    interrupt::supervisor::{Exception, Interrupt},
+    register::scause::Trap,
+};
 
 use crate::{
     arch::{
         cpu::extension::{has_extensions, IsaExtensions},
-        trap::{call_irq_callback_functions_by_scause, RawUserContext, TrapFrame, SSTATUS_FS_MASK},
+        trap::{call_timer_soft_or_ext_irq_callbacks, RawUserContext, TrapFrame, SSTATUS_FS_MASK},
     },
     cpu::PrivilegeLevel,
     user::{ReturnReason, UserContextApi, UserContextApiInternal},
@@ -125,7 +128,6 @@ impl CpuException {
             InstructionPageFault => Self::InstructionPageFault(stval),
             LoadPageFault => Self::LoadPageFault(stval),
             StorePageFault => Self::StorePageFault(stval),
-            Unknown => Self::Unknown,
         }
     }
 }
@@ -186,11 +188,14 @@ impl UserContextApiInternal for UserContext {
             self.user_context.run();
 
             let scause = riscv::register::scause::read();
-            match scause.cause() {
+            let Ok(cause) = Trap::<Interrupt, Exception>::try_from(scause.cause()) else {
+                self.exception = Some(CpuException::Unknown);
+                break ReturnReason::UserException;
+            };
+            match cause {
                 Trap::Interrupt(interrupt) => {
-                    call_irq_callback_functions_by_scause(
+                    call_timer_soft_or_ext_irq_callbacks(
                         &self.as_trap_frame(),
-                        scause.bits(),
                         interrupt,
                         PrivilegeLevel::User,
                     );
